0dcbfb68e5b88a8de29220e5689513cd3b9155c1
shienasnow <shiena0302@gmail.com>
2024-11-20 17:52:23 +0900

    post-load test

---
+++ hooks/tk-multi-loader2/post_load.py
@@ -0,0 +1,15 @@
+import sgtk
+
+HookBaseClass = sgtk.get_hook_baseclass()
+
+class PostLoadHook(HookBaseClass):
+    def post_load(self, context, file_path, publish_type, sequence=None):
+        """
+        Your custom post-load logic here
+        """
+        # 예시: 로드된 파일 경로 출력
+        self.logger.debug("Loading completed: %s" % file_path)
+        
+        # 여기에 원하는 커스텀 코드 작성
+        
+        return True
