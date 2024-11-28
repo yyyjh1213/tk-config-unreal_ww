94f321290b5b7493237b4fe24ab03bf8b0689fba
shienasnow <shiena0302@gmail.com>
2024-11-28 10:51:15 +0900

    add event handler UE_PYTHONPATH

---
+++ hooks/packages/win/rename.py
@@ -0,0 +1,13 @@
+import unreal
+
+def post_import_asset(asset):
+    # 어셋 처리 (예: 경로 변경, 이름 재지정 등)
+    print("@"*20)
+    if isinstance(asset, unreal.StaticMesh):
+        new_path = "/Game/MyStaticMeshes/"
+        asset_name = "CustomStaticMeshName"
+        unreal.EditorAssetLibrary.rename_asset(asset.get_path_name(), f"{new_path}{asset_name}")
+        print(f"Asset renamed and path set: {new_path}{asset_name}")
+    print("@"*20)
+# 연결
+unreal.EditorAssetLibrary.on_asset_imported().add_callable(post_import_asset)
