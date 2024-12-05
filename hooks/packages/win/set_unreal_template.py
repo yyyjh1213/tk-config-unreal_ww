a67d5dd5d21293a841acac8db4008269c657ba5d
shienasnow <shiena0302@gmail.com>
2024-12-05 18:57:31 +0900

    modify modules3

--- hooks/packages/win/set_unreal_template.py
+++ hooks/packages/win/set_unreal_template.py
@@ -12,16 +12,13 @@ def get_engine_dir():
     print("엔진과 템플릿 경로 찾기")
     # engine_dir = unreal.SystemLibrary.get_engine_directory()
 
-    # 현재 엔진 인스턴스 가져오기
-    engine = sgtk.platform.current_engine()
+    # # 현재 엔진 인스턴스 가져오기
+    # engine = sgtk.platform.current_engine()
     
-    if engine:
-        # 엔진의 설치 경로 가져오기
-        engine_path = engine.disk_location
-        print(engine_path)
-
-
-
+    # if engine:
+    #     # 엔진의 설치 경로 가져오기
+    #     engine_path = engine.disk_location
+    #     print(engine_path)
 
     ##########################################################
     # print(f"Engine Directory : {engine_dir}")
@@ -34,25 +31,29 @@ def get_engine_dir():
 def get_app_info():
     # 현재 컨텍스트 가져오기
     current_context = sgtk.platform.current_bundle().context
-    
+    print("()"*20)
+    print(f"current_context : {current_context}")
+
     # 현재 엔진 가져오기
     engine = sgtk.platform.current_engine()
-    
-    if engine:
-        print(f"현재 엔진: {engine.name}")
-        print(f"엔진 경로: {engine.disk_location}")
+    if engine is None:
+        raise RuntimeError("SGTK Engine is not initialized!")
+
+    # if engine:
+    #     print(f"현재 엔진: {engine.name}")
+    #     print(f"엔진 경로: {engine.disk_location}")
         
-        # 모든 등록된 앱 정보 출력
-        print("\n등록된 앱들:")
-        for app_instance_name, app in engine.apps.items():
-            print(f"\n앱 인스턴스: {app_instance_name}")
-            print(f"앱 경로: {app.disk_location}")
-            print(f"앱 설정: {app.configuration}")
+    #     # 모든 등록된 앱 정보 출력
+    #     print("\n등록된 앱들:")
+    #     for app_instance_name, app in engine.apps.items():
+    #         print(f"\n앱 인스턴스: {app_instance_name}")
+    #         print(f"앱 경로: {app.disk_location}")
+    #         print(f"앱 설정: {app.configuration}")
             
-            # 앱의 환경 설정 가져오기
-            env = app.get_setting("env")
-            if env:
-                print(f"환경 설정: {env}")
+    #         # 앱의 환경 설정 가져오기
+    #         env = app.get_setting("env")
+    #         if env:
+    #             print(f"환경 설정: {env}")
 
 
 def move(src_dir, dst_dir):
